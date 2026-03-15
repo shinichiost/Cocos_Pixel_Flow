4f27ebae441368ef38d4dd1def13a5121ab64de3
taitt <taitt@higame.com.vn>
2026-03-15 21:36:48 +0700

    no message

---
+++ assets/Scripts/level/BoardSpawner.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('BoardSpawner')
+export class BoardSpawner extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
